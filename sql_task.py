81e97d5ee0600be1202ea538dce0aa08145c1935
Andrew Lange <andrew.lange93@gmail.com>
2019-04-30 19:00:21 -0400

    create connenction() function & prepared query_one() & query_two()

--- sql_task.py
+++ sql_task.py
@@ -1,8 +1,40 @@
 import sys
 import os
+import sqlite3
+from sqlite3 import Error
+
+
+def create_connection(db_file):
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
+
+    return None
+
+
+def query_one(conn):
+    cur = conn.cursor()
+    cur.execute("select * from people;")
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+def query_one(conn):
+    cur = conn.cursor()
+    cur.execute("select * from people;")
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
 
 if len(sys.argv) == 1:
-    print(""""
+    print("""
     This is a Python module called "sql_task" used query a SQLiteDB database.
     
     Usage:
@@ -19,6 +51,22 @@ if len(sys.argv) == 1:
             number of sites they've visited.
     """)
 elif len(sys.argv) == 2:
-    print("will proccess either --task-one or --task-two")
+    current_dir = os.path.dirname(__file__)
+    database_path = os.path.join(current_dir, 'testdb.db')
+    conn = create_connection(database_path)
+    with conn:
+        for param in sys.argv:
+            if param == "--task-one":
+                print("processing --task-one ")
+                query_one(conn)
+            elif param == "--task-two":
+                print("processing  --task-two")
+                query_two(conn)
+            elif "sql_task" in param:
+                pass
+            else:
+                print("Parameter " + str(param) + " not recognized. " \
+                      + "Please try again or type 'python -m sql_task' for help.")
 else:
-    print("Too many parameters entered. Type 'python -m sql_task' for help")
+    print("Too many parameters entered.\nPlease try again or type" \
+          + " 'python -m sql_task' for help.")
