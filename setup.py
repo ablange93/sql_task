94055487b9e47f2dca2416ba4dfd431deeddbc70
Andrew Lange <andrew.lange93@gmail.com>
2019-05-04 16:22:20 -0400

    modified setup.py file

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='sql_task',
     version='0.1',
-    description='Cherre programming exercise',
+    description='Python application for querying SQLite3 database',
     author='Andrew Lange',
     classifiers=['License :: OSI Approved :: MIT License',
                  'Programming Language :: Python :: 3.6'
