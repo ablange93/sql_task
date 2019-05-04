725b60b0a98af1ce807edcdd0be292302498a97a
Andrew Lange <andrew.lange93@gmail.com>
2019-05-04 16:08:14 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sql_task',
+    version='0.1',
+    description='Cherre programming exercise',
+    author='Andrew Lange',
+    classifiers=['License :: OSI Approved :: MIT License',
+                 'Programming Language :: Python :: 3.6'
+    ]
+)
